7607a48fd448890cb8c8b0917d9ad92b83ef06ed
argen23 <argenomarbekov192@gmail.com>
2021-08-19 18:35:19 +0300

    Initial commit

---
+++ app/src/main/java/com/example/taskapp/ui/profile/ProfileFragment.java
@@ -0,0 +1,60 @@
+package com.example.taskapp.ui.profile;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.provider.MediaStore;
+import android.service.media.MediaBrowserService;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.example.taskapp.R;
+
+import static android.app.Activity.RESULT_OK;
+
+
+public class ProfileFragment extends Fragment {
+    private ImageView ava;
+    private ActivityResultLauncher<String> arl;
+    private Uri uri;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ava = view.findViewById(R.id.avatar_img);
+        ava.setOnClickListener(v -> {
+            Intent in = new Intent();
+            in.setAction(Intent.ACTION_PICK);
+            in.setType("image/*");
+            startActivityForResult(in,1);
+
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK){
+            uri = data.getData();
+            ava.setImageURI(uri);
+            Glide.with(requireContext()).load(uri).circleCrop().into(ava);
+        }
+    }
+}
